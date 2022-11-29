89ba37436db04437dea242195764c743814e5c90
thelenaza <kaxtie07@gmail.com>
2022-11-29 11:29:28 +0700

    update

--- lib/list_view_menu.dart
+++ lib/list_view_menu.dart
@@ -1,11 +1,10 @@
-
 import 'package:flutter/material.dart';
 
-class ListViewMenuPage extends StatelessWidget{
-  const ListViewMenuPage({Key?key}):super(key:key);
-  
+class ListViewMenuPage extends StatelessWidget {
+  const ListViewMenuPage({Key? key}) : super(key: key);
+
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
@@ -15,15 +14,19 @@ class ListViewMenuPage extends StatelessWidget{
         child: ListView.separated(
           itemCount: 4,
           separatorBuilder: (_,index){
-            return Divider(color: Colors.pink);
+            return Divider(color: Colors.red);
           },
-          itemBuilder: (context,index) {
+          itemBuilder: (context,index){
             return ListTile(
+                title: Text('Menu-1'),
+                subtitle: Text('SUB-1'),
+                leading: CircleAvatar(child: Icon(Icons.person)),
+                trailing: Icon(Icons.arrow_right),
                 onTap: () {
                   debugPrint('MENU-1');
                 }
             );
-          }
+          },
         ),
       ),
     );
